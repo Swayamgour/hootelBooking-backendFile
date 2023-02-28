6bc2e802ebcb03f2e323dc28589de946450e7476
Swayamgour <119596165+Swayamgour@users.noreply.github.com>
2023-02-28 16:12:25 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,76 @@
+import express from "express"
+import cors from "cors"
+import mongoose from "mongoose"
+
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded())
+app.use(cors())
+
+mongoose.connect("mongodb://127.0.0.1:27017/myLogRegisterDb", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, () => {
+    console.log("DB connected")
+})
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    password: String
+})
+
+const User = new mongoose.model("User", userSchema)
+
+
+app.get("/myLogRegisterDb/cities", (req, res) => {
+    cities.find( (err, cities) => {
+        if (err) {
+            res.status(500).send(err)
+        } else {
+            res.status(200).send(cities);
+        }
+    })
+})
+
+app.post("/Login", (req, res) => {
+    const { email, password } = req.body
+    User.findOne({ email: email }, (err, user) => {
+        if (user) {
+            if (password === user.password) {
+                res.send({ message: "Login Successfull", user: user })
+            } else {
+                res.send({ message: "Password didn't match" })
+            }
+        } else {
+            res.send({ message: "User not registered" })
+        }
+    })
+})
+
+app.post("/Restriction", (req, res) => {
+    const { name, email, password } = req.body
+    User.findOne({ email: email }, (err, user) => {
+        if (user) {
+            res.send({ message: "User already registerd" })
+        } else {
+            const user = new User({
+                name,
+                email,
+                password
+            })
+            user.save(err => {
+                if (err) {
+                    res.send(err)
+                } else {
+                    res.send({ message: "Successfully Registered" })
+                }
+            })
+        }
+    })
+
+})
+
+app.listen(9002, () => {
+    console.log("BE started at port 9002")
+})
